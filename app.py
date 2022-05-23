2cf07ccfa4ff263f0afe0de22fbaf934b6423d76
Rafi <rafirasheed.tc99@gmail.com>
2022-05-23 06:30:30 +0530

    updated proc

--- app.py
+++ app.py
@@ -1,17 +1,25 @@
 
+from ipaddress import ip_address
 from flask import Flask
 from flask import Flask, redirect, url_for, request, jsonify
 import pickle 
 import pandas as pd 
 import warnings
+
+from requests import NullHandler
 warnings.filterwarnings("ignore", category=UserWarning)
 import firebase_admin
+from firebase_admin import db
 
 app = Flask(__name__)
-# cred_obj = firebase_admin.credentials.Certificate('....path to file')
-# default_app = firebase_admin.initialize_app(cred_object, {
-# 	'databaseURL':databaseURL
-# 	})
+
+# cred = credentials.Certificate("asthma-pred-firebase-adminsdk-kokv3-19683e0cac.json")
+# firebase_admin.initialize_app(cred)
+cred_obj = firebase_admin.credentials.Certificate('asthma-pred-firebase-adminsdk-kokv3-19683e0cac.json')
+default_app = firebase_admin.initialize_app(cred_obj, {
+	'databaseURL':'https://asthma-pred-default-rtdb.firebaseio.com',
+	})
+
 
 filename = "DT.sav"
 
@@ -19,17 +27,72 @@ filename = "DT.sav"
 def index():
     return "Web App with Python Flask!"
 
-@app.route('/value', methods=["POST"])
-def storeValue():
-    return "store"
+@app.route('/postplain/', methods= ["POST", 'GET'])
+def postplain():
+    content_type = request.headers.get('Content-Type')
+    if (content_type == 'application/json'):
+        json = request.json
+        print(json)
+        return "Done"
+    else:
+        return 'Content-Type not supported!'
 
+
+
+@app.route('/value', methods=["GET"])
+def storeValue():
+    ref = db.reference("/")
+    ref.push({
+    "data": {
+        "673640": {
+            "timestamp": "1",
+            "temp": "34",
+            "hum": "72",
+            "pm2": "49",
+    }
+  }
+})
+    return "Done"
+
+@app.route('/getvalue', methods=["GET"])
+def getValue():
+    pin = '673640'
+    ref = db.reference("/")
+    j_value = ref.get()
+    for i in j_value:
+        a = j_value[i]['data'].get(pin)
+        print(a)
+    if a == None:
+        print(a)
+        return('no pincode')
+    return(a)
+
+@app.route('/updatevalue', methods=["GET"])
+def updatevalue():
+    pin = '673640'
+    ref = db.reference("/")
+    j_value = ref.get()
+    for i in j_value:
+        a = j_value[i]['data'].get(pin)
+        print(a)
+    if a == None:
+        print(a)
+        return('no pincode')
+    
+    return(a)
 
 @app.route('/predict', methods=["POST"])
 def predict():
     pincode = request.form['pin']
-    temperature = 35 #request.form['temp']
-    humidity = 80#request.form['hum']
-    pm2 = 19 #request.form['pm']
+    print("Pin ", pincode)
+    ref = db.reference("/-N2fNxS3nj_2fiKPVv2A/data/"+pincode)
+    value = ref.get()
+    if value == None:
+        print("No value in")
+        return "no data"
+    temperature = value["temp"]
+    humidity = value["hum"]
+    pm2 = value["pm2"]
 
     print("got deatails ", pincode," ", temperature," ", humidity," ", pm2)
     li =  []
@@ -52,5 +115,5 @@ def predict():
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port="6000", host="0.0.0.0")
 
